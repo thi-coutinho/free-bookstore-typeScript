2d1289d282e79de45caa863cca9ab2f4aa46586e
thi-coutinho <thi.coutinho.santos@gmail.com>
2023-04-10 12:12:36 -0300

    build: ts config, installed libraries

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import "express-async-errors";
+import express, { json } from "express";
+import cors from "cors";
+// import routes from "./routes/index.js";
+// import { handleApplicationErrors } from "./middlewares/errorMiddleware.js";
+
+const app = express();
+app.use(json());
+app.use(cors());
+// app.use(routes);
+// app.use(handleApplicationErrors);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running in port: ${port}`));
